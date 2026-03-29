512b42de4d0729cc6d6de55940f7fa95362b3555
Abdul <abdul@example.com>
2026-03-29 12:50:07 +0100

    sreen fix

--- app/(app)/layout.jsx
+++ app/(app)/layout.jsx
@@ -10,8 +10,8 @@ export default function AppLayout({ children }) {
             <div className="flex flex-1 relative w-full overflow-hidden">
                 <Sidebar />
                 <main
-                    className="flex-1 w-full pb-32 md:pb-0 overflow-y-auto overflow-x-hidden scroll-smooth transition-all duration-300"
-                    id="main-content"
+                // className="flex-1 w-full pb-32 md:pb-0 overflow-y-auto overflow-x-hidden scroll-smooth transition-all duration-300"
+                // id="main-content"
                 // style={{
                 //     WebkitOverflowScrolling: "touch",
                 //     overscrollBehaviorY: "contain"
